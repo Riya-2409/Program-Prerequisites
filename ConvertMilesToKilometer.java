173f5441c4e8a6bc259d3bb4fdb3e238acf588e8
Riya <sonriya2409@gmail.com>
2025-01-08 01:01:42 +0530

    Question 10

---
+++ ConvertMilesToKilometer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class ConvertKilometerstoMiles{
+public static void main(String args[]){
+Scanner kb=new Scanner(System.in);
+double kilometer=kb.nextDouble();
+
+double miles=kilometer*0.621371;
+
+
+System.out.println(miles);
+}
+}
