f872c38e8b24244d4955896de23fc9ea4fd041e2
Riya <sonriya2409@gmail.com>
2025-01-08 00:16:47 +0530

    Question 4

---
+++ AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class AreaOfCircle{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+double rad = sc.nextDouble();
+double area = 3.14*(rad*rad);
+
+System.out.println("Area of Circle = "+area);
+}
+}
